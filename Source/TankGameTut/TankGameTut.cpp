c1a9c879b253665d3b8e43d3bc3920d92b756a85
wolfskrieg <milosdanilovic123@gmail.com>
2016-12-30 13:17:25 +0100

    Initial project commit

---
+++ Source/TankGameTut/TankGameTut.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankGameTut.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TankGameTut, "TankGameTut" );
---
+++ Source/TankGameTut/TankGameTutGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TankGameTutGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKGAMETUT_API ATankGameTutGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
